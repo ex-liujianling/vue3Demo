c132bdf6b1186b02da116696b513c86ee609f6f3
liujianling <1714538535@qq.com>
2023-06-04 23:07:05 +0800

    代码体骄傲

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// import Vue from 'vue';
+// import Element from 'element-ui';
+// Vue.use(Element, { size: 'small', zIndex: 3000 });
 createApp(App).mount('#app')
